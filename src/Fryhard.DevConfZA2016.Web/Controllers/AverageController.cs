84d50a11f4f0edd3abaea0b1bd74e4d628081504
Brendan Fry <brendan@jagmethod.com>
2016-02-29 10:57:17 +0200

    Update presentationm change views to not use _ViewStart

---
+++ src/Fryhard.DevConfZA2016.Web/Controllers/AverageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fryhard.DevConfZA2016.Web.Controllers
+{
+    public class AverageController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
