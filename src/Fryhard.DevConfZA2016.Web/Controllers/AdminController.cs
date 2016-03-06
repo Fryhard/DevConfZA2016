82bca2db8e9c19b48ad6e5ce3824a68ccac4a5d7
Brendan Fry <brendan@jagmethod.com>
2016-03-06 15:23:06 +0200

    Track missing files

---
+++ src/Fryhard.DevConfZA2016.Web/Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using Fryhard.DevConfZA2016.Common;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fryhard.DevConfZA2016.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Stop()
+        {
+            Control control = new Control()
+            {
+                Stop = true
+            };
+
+            BusHost.Publish<Control>(control, BusTopic.Control);
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public ActionResult Start()
+        {
+            Control control = new Control()
+            {
+                Start = true
+            };
+
+            BusHost.Publish<Control>(control, BusTopic.Control);
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public ActionResult Reset()
+        {
+            Control control = new Control()
+            {
+                Reset = true
+            };
+
+            BusHost.Publish<Control>(control, BusTopic.Control);
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
